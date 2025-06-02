2dc2cb622d7f36f9bf86e30fc9b44836fe5b086b
harshu1210 <charshal40@gmail.com>
2025-06-02 20:10:34 +0530

    route-update

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -14,30 +14,30 @@ import { UserService } from 'src/app/services/user.service';
 export class LoginComponent implements OnInit {
   formMessage: string = "Login User";
   formCreation: any[] = [
-      {
-        label: "User Emai", key: "email", value: '', type: 'text', validation: ['required','email'],
-        options: [], disabled:false
-      },
-      {
-        label: "Password", key: "passwordHash", value: '', type: 'password', validation: ['required'],
-        options: [] , disabled:false
-      }
-    ];
-  token:string="";
+    {
+      label: "User Emai", key: "email", value: '', type: 'text', validation: ['required', 'email'],
+      options: [], disabled: false
+    },
+    {
+      label: "Password", key: "passwordHash", value: '', type: 'password', validation: ['required'],
+      options: [], disabled: false
+    }
+  ];
+  token: string = "";
 
-  constructor(private userService:UserService, private authService:AuthService, private toasterService:ToasterService, private router:Router, private storageService:StorageService) { }
+  constructor(private userService: UserService, private authService: AuthService, private toasterService: ToasterService, private router: Router, private storageService: StorageService) { }
 
   ngOnInit(): void {
-  
+
   }
 
   submitSubmission(event: any) {
-    this.userService.loginUser(event).pipe(finalize(() => 
-      this.authToken()  
-    )).subscribe((data:any)=>{
+    this.userService.loginUser(event).pipe(finalize(() =>
+      this.authToken()
+    )).subscribe((data: any) => {
       this.token = data.auth;
       this.toasterService.success("User Logged In Successfully")
-    },(error:any)=>{
+    }, (error: any) => {
       this.toasterService.error(error.message)
     })
   }
@@ -45,7 +45,7 @@ export class LoginComponent implements OnInit {
   authToken() {
     if (this.token) {
       this.authService.startSession(this.token)
-      this.storageService.extractToken().includes("CUSTOMER") ? this.router.navigate(["packageTrackingPage"]) : this.router.navigate(["customerPage"])
+      this.storageService.extractToken().includes("CUSTOMER") ? this.router.navigate(["packageTrackingPage"]) : (this.storageService.extractToken().includes("SUPERADMIN") ? this.router.navigate(["userPage"]) : this.router.navigate(["customerPage"]))
     }
   }
 }
